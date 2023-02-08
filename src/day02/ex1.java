45303ef7528a51a79a9b6d2879378a1e762e0ca5
yeong seop <sim072900@gmail.com>
2023-02-08 09:52:14 +0900

    Initial commit

---
+++ src/day02/ex1.java
@@ -0,0 +1,26 @@
+package day02;
+
+public class ex1 {
+    public static void main(String[] args) {
+        // 문제 1
+        System.out.println("총점 : " + ( 30+70+80 ) );
+        System.out.println("평균 : " + ( 30+70+80)/3 );
+        // 문제 2
+        System.out.println("사다리꼴 넓이 : "+ ( ( 30*20)*10/2 ) );
+        // 문제 3
+
+
+
+        // 문제 4
+        System.out.println( 3753%2 == 1 ? "홀수" : "짝수" );
+        // 문제 5
+        System.out.println( 2056%2 == 0 ? "짝수" : "홀수" );
+        // 문제 6
+        System.out.println( 7792%7 == 0 ? "7배수 o" : "7배수 x");
+        // 문제 7
+        System.out.println( 7792%3 == 1 ? "3배수 o" : "3배수 x");
+
+
+    }
+}
+
